b693080b0571e6aa284308256f072690980bdde1
Manoj <manoj612kumar@gmail.com>
2023-10-09 10:35:34 +0530

    init commit

--- JavaConcepts/Day8-JavaConcepts/FinalClassTest.java
+++ JavaConcepts/Day8-JavaConcepts/FinalClassTest.java
@@ -2,7 +2,7 @@ final class FinalClass {
 }
 
 // Cannot inherit a final class
-public class FinalClassTest {// extends FinalClass //
+public class FinalClassTest /* extends FinalClass */{
     public static void main(String[] args) {
 
     }
