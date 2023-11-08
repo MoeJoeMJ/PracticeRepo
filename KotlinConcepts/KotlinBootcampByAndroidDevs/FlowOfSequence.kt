a828032f484160f9bca88316d6637f421b5490ec
Manoj <manoj612kumar@gmail.com>
2023-11-08 19:25:47 +0530

    init commit

--- KotlinConcepts/KotlinBootcampByAndroidDevs/FlowOfSequence.kt
+++ KotlinConcepts/KotlinBootcampByAndroidDevs/FlowOfSequence.kt
@@ -4,5 +4,27 @@ fun main() {
         println("current: $it")
         it
     }
-
+    println("This doesn't execute the filter. $filtered")
+    println("This doesn't either. ${filtered.first()}")
+    println("This doesn't either. ${filtered.last()}")
+    println("-----------------")
+    println("This does. ${filtered.toList()}")
 }
+
+/*
+OUTPUT:
+This doesn't execute the filter. kotlin.sequences.TransformingSequence@25f38edc
+current: Elvis
+This doesn't either. Elvis
+current: Elvis
+current: Elton
+current: Prince
+current: Freddie
+This doesn't either. Freddie
+-----------------
+current: Elvis
+current: Elton
+current: Prince
+current: Freddie
+This does. [Elvis, Elton, Prince, Freddie]
+*/
