ec7a970468c9da43ed27a7c068548b7bf856e0c3
Manoj <manoj612kumar@gmail.com>
2023-11-10 12:18:37 +0530

    init commit

--- KotlinConcepts/KotlinBootcampByAndroidDevs/Practice.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val fruits = listOf("banana", "avocado", "apple", "kiwi")
-    fruits
-        .filter { it.startsWith("a") }
-        .sortedBy { it }
-        .forEach { println(it.uppercase()) }
-}
